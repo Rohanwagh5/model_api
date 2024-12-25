0aa3a9698a77ba9487bdd2104ab0e6ce14f623f4
Rohanwagh5 <129928120+Rohanwagh5@users.noreply.github.com>
2024-12-25 13:45:23 +0530

    Update model_api.py

--- model_api.py
+++ model_api.py
@@ -1,3 +1,4 @@
+
 # from flask import Flask, request, jsonify
 # import joblib
 # import numpy as np
@@ -7,22 +8,33 @@
 # # Load the trained model
 # model = joblib.load('knn_model2.joblib')
 
-# @app.route('/', methods=['POST'])
+# # Home route for GET requests
+# @app.route('/', methods=['GET'])
+# def home():
+#     return "Heart Disease Prediction API is running! Use the /predict endpoint to make predictions.", 200
+
+# # Predict route for POST requests
+# @app.route('/predict', methods=['POST'])
 # def predict():
-#     # Parse incoming JSON data
-#     data = request.json
-#     features = np.array([[
-#         data['age'], data['sex'], data['cp'], data['trestbps'], data['chol'],
-#         data['fbs'], data['restecg'], data['thalach'], data['exang'], data['oldpeak'],
-#         data['slope'], data['ca'], data['thal']
-#     ]])
-#     # Make a prediction using the loaded model
-#     prediction = model.predict(features)
-#     return jsonify({'prediction': int(prediction[0])})
+#     try:
+#         # Parse incoming JSON data
+#         data = request.json
+#         features = np.array([[  
+#             data['age'], data['sex'], data['cp'], data['trestbps'], data['chol'],
+#             data['fbs'], data['restecg'], data['thalach'], data['exang'], data['oldpeak'],
+#             data['slope'], data['ca'], data['thal']
+#         ]])
+#         # Make a prediction using the loaded model
+#         prediction = model.predict(features)
+#         return jsonify({'prediction': int(prediction[0])}), 200
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 400
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
+
+import os
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
@@ -32,28 +44,21 @@ app = Flask(__name__)
 # Load the trained model
 model = joblib.load('knn_model2.joblib')
 
-# Home route for GET requests
-@app.route('/', methods=['GET'])
-def home():
-    return "Heart Disease Prediction API is running! Use the /predict endpoint to make predictions.", 200
-
-# Predict route for POST requests
-@app.route('/predict', methods=['POST'])
+@app.route('/', methods=['POST'])
 def predict():
-    try:
-        # Parse incoming JSON data
-        data = request.json
-        features = np.array([[  
-            data['age'], data['sex'], data['cp'], data['trestbps'], data['chol'],
-            data['fbs'], data['restecg'], data['thalach'], data['exang'], data['oldpeak'],
-            data['slope'], data['ca'], data['thal']
-        ]])
-        # Make a prediction using the loaded model
-        prediction = model.predict(features)
-        return jsonify({'prediction': int(prediction[0])}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    # Parse incoming JSON data
+    data = request.json
+    features = np.array([[  # Prepare feature array
+        data['age'], data['sex'], data['cp'], data['trestbps'], data['chol'],
+        data['fbs'], data['restecg'], data['thalach'], data['exang'], data['oldpeak'],
+        data['slope'], data['ca'], data['thal']
+    ]])
+    prediction = model.predict(features)  # Make prediction
+    return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the PORT environment variable from Render (default to 5000 if not set)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
