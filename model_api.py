01ce7a857e316f92ffb783a445aa21ff8131f450
Rohanwagh5 <129928120+Rohanwagh5@users.noreply.github.com>
2024-12-25 14:22:53 +0530

    Update model_api.py with cors importing

--- model_api.py
+++ model_api.py
@@ -34,12 +34,43 @@
 #     app.run(debug=True)
 
 
+# import os
+# from flask import Flask, request, jsonify
+# import joblib
+# import numpy as np
+
+# app = Flask(__name__)
+
+# # Load the trained model
+# model = joblib.load('knn_model2.joblib')
+
+# @app.route('/', methods=['POST'])
+# def predict():
+#     # Parse incoming JSON data
+#     data = request.json
+#     features = np.array([[  # Prepare feature array
+#         data['age'], data['sex'], data['cp'], data['trestbps'], data['chol'],
+#         data['fbs'], data['restecg'], data['thalach'], data['exang'], data['oldpeak'],
+#         data['slope'], data['ca'], data['thal']
+#     ]])
+#     prediction = model.predict(features)  # Make prediction
+#     return jsonify({'prediction': int(prediction[0])})
+
+# if __name__ == '__main__':
+#     # Get the PORT environment variable from Render (default to 5000 if not set)
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port, debug=True)
+
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import flask_cors
 import joblib
 import numpy as np
 
 app = Flask(__name__)
+
+# Enable CORS for all domains
+CORS(app)
 
 # Load the trained model
 model = joblib.load('knn_model2.joblib')
@@ -60,5 +91,6 @@ if __name__ == '__main__':
     # Get the PORT environment variable from Render (default to 5000 if not set)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
